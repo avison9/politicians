baf158d2762d12896cc6d04ccc53ac49ea4d08af
root <root@avisonlivingstone.pserver.space>
2024-06-15 13:54:18 +0500

    creating initial views for pages

--- politics/accounts/views.py
+++ politics/accounts/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import RegisterForm, LoginForm
 
-# Create your views here.
+# Home page
+def home(request):
+    return render(request,'accounts/index.html' )
+
+# Registration a user
+def register(request):
+
+    form = RegisterForm()
+
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+
+            form.save()            
+
+            # return redirect('home')
+
+    context = {'form': form}
+
+    return render(request, 'accounts/registration.html', context)
